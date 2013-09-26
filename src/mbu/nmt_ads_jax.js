91719562f8dfc1dbf3cd52dd1f86f8e6aa3b15a2
Duane Jennings <duane.jennings@morris.com>
2013-09-25 22:31:21 -0400

    Added fix to handle trailing slash paths

--- src/mbu/nmt_ads_jax.js
+++ src/mbu/nmt_ads_jax.js
@@ -2,7 +2,7 @@
 /***
  * Provides data and methods for serving ads.
  * @author: Duane.Jennings@niit-mediatech.com
- * @version: 201309201044:443263
+ * @version: 201309201254:443263
  * 
  */
 var NMTdata = NMTdata || {};
@@ -33,7 +33,7 @@ var NMTdata = NMTdata || {};
 
 ////////////////////////////////////////////////////////////////////////
 /***
- * mapping_version: 201309201043:444002
+ * mapping_version: 201309201247:444002
  */
 dfp_adunit_prefix = '/11365842/jacksonville.com',
         adunitPrefixDomainMappings = [
@@ -130,7 +130,13 @@ dfp_adunit_prefix = '/11365842/jacksonville.com',
         dfp_adunit = data.processMapping(adunitURLMappings, document.URL, dfp_adunit);
 
         // dfp_ccc default value
-        dfp_ccc = data.pathnames[data.pathnames.length - 1];
+        // Paths that have trailing slash will leave the last path element empty.
+        // Let's check for this case and go back one more for the ccc value.
+        if (data.pathnames.length > 1 && data.pathnames[data.pathnames.length-1] == '') {
+            dfp_ccc = data.pathnames[data.pathnames.length - 2];
+        } else {
+            dfp_ccc = data.pathnames[data.pathnames.length - 1];
+        }
         
         // Process Path mappings for dfp_ccc
         dfp_ccc = data.processMapping(cccPathMappings, window.location.pathname, dfp_ccc);
@@ -161,12 +167,11 @@ dfp_adunit_prefix = '/11365842/jacksonville.com',
         }
 
         return { // return object
-            dfp_nmt_mapping_version: '201309201043:444002',
-            dfp_nmt_ads_version: '201309201044:443263',
+            dfp_nmt_mapping_version: '201309201247:444002',
+            dfp_nmt_ads_version: '201309201254:443263',
             dfp_adunit_prefix: dfp_adunit_prefix,
             dfp_adunit: dfp_adunit,
             dfp_ccc: data.escapeHtml(dfp_ccc)
         };
 
     }());
-
